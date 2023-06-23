fcab03a59c17b6481f63784de76d625657eebefb
aruturu24 <aruturu24@hotmail.com>
2023-06-23 15:54:11 -0300

    FEAT: added social medias and email, added media query to larger screens

--- src/app/components/contact/index.tsx
+++ src/app/components/contact/index.tsx
@@ -1,9 +1,19 @@
+import Github from '../icons/github'
+import Instagram from '../icons/instagram'
+import Linkedin from '../icons/linkedin'
+import Twitter from '../icons/twitter'
 import './contact.scss'
 
 export default function Contact() {
   return(
     <div id='contact'>
-      <button>Contact me</button>
+      <div id='social-medias'>
+        <a href='https://github.com/aruturu24'><Github/></a>
+        <a href='https://linkedin.com/in/aruturu24'><Linkedin/></a>
+        <a href='https://twitter.com/aruturu24'><Twitter/></a>
+        <a href='https://instagram.com/aruturu24'><Instagram/></a>
+      </div>
+      <button><a href='mailto:aruturu24@hotmail.com'>Contact me</a></button>
     </div>
   )
 }
