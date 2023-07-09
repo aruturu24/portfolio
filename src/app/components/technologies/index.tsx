7f08df366db79336caee4d850d12b4262abcf9e4
Arthur Gabryel <aruturu24@hotmail.com>
2023-07-09 03:13:58 +0000

    FEAT: Technologies content

--- src/app/components/technologies/index.tsx
+++ src/app/components/technologies/index.tsx
@@ -1,10 +1,26 @@
-import './technologies.scss'
+import Mysql from "../icons/mysql";
+import Nodejs from "../icons/nodejs";
+import React from "../icons/react";
+import "./technologies.scss";
 
 export default function Technologies() {
-  return(
-    <div id='technologies'>
-      <h3 className='section-title'>Technologies</h3>
-      <div className='technology-info'></div>
+  return (
+    <div id="technologies">
+      <h3 className="section-title">Technologies</h3>
+      <div id="technologies-group">
+        <div className="technology-info">
+          <React />
+          <span>React.JS</span>
+        </div>
+        <div className="technology-info">
+          <Nodejs />
+          <span>NodeJS</span>
+        </div>
+        <div className="technology-info">
+          <Mysql />
+          <span>MySQL</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
